336784809591911f3038decb46ec486c4375eabc
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-22 16:31:59 +0100

    Update 1_Gas Reservoir.py

--- 1_Gas Reservoir.py
+++ 1_Gas Reservoir.py
@@ -1,8 +1,9 @@
+import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from tabulate import tabulate
-import streamlit as st
+
 
 st.title("IPR Calculation")
 st.markdown("Forcheimer Reservoir Model")
