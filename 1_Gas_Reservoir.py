17197f59e056bc4e1a0bade1bea2da10babb81d7
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-23 19:47:45 +0100

    Update 1_Gas_Reservoir.py

--- 1_Gas_Reservoir.py
+++ 1_Gas_Reservoir.py
@@ -5,7 +5,7 @@ from scipy.optimize import curve_fit
 from tabulate import tabulate
 
 # Definition of the quadratic curve equation (Inflow Performance Relationship - IPR)
-def curve_IPR(Q, a, b, Pws):
+def curve_IPR(Q, a, b):
     return np.sqrt(-a * Q ** 2 - b * Q + Pws**2)
 
 # Function to collect input data
@@ -25,31 +25,33 @@ def collect_data():
         Q = st.number_input("Rate (Q) (km3/d)", key=f"Q_{i}")
 
         data.append((date, comment, Pwf, Q))
-
+        
+    data.append((date, comment, Pws, Pwf, Q))
+    
     # Print data as a table
     st.subheader("Input Data Summary")
     headers = ["Date", "Comment", "Pwf (bar)", "Rate (km3/d)"]
     st.write(tabulate(data, headers=headers, tablefmt="grid"))
 
-    return Pws, data
+    return data
 
 def main():
     st.title("IPR Curve Fitting")
 
     # Collect test data
-    Pws, data = collect_data()
+    data = collect_data()
 
     if data:
         # Convert rate from km3/d to Sm3/day
         for i in range(len(data)):
             data[i] = (data[i][0], data[i][1], data[i][2], data[i][3], data[i][3] * 1e3)
-
-        # For Pws
-        data.append(('', '', Pws, 0, 0))  # Add Pws to the data list
-
+     
+        Pws=data[0][2]
+        data2 = [(Pws,0)]
+                        
         # Convert data into arrays
-        Q_data = np.array([d[4] for d in data])
-        P_data = np.array([d[3] for d in data])
+        Q_data = np.array([d[4] for d in data]+[d[1] for d in data2])
+        P_data = np.array([d[3] for d in data]+[d[0] for d in data2])
 
         # Perform curve fitting
         initial_guess = [3.75e-9, 4.17e-4]  # Initial guess for the parameters a, b, and c
@@ -63,7 +65,7 @@ def main():
 
         # Range of points for extrapolation of the curve
         Q_range = np.linspace(0, np.max(Q_data), 500)
-        Pwf_fit = curve_IPR(Q_range, a_fit, b_fit, Pws)  # Assuming constant Pws for simplicity
+        Pwf_fit = curve_IPR(Q_range, a_fit, b_fit)  
 
         # Plot
         st.subheader("IPR Curve")
