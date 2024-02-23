484f12e125e333814b138c9ec61639b4693a02df
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-23 09:31:48 +0100

    Update 1_Gas_Reservoir.py

--- 1_Gas_Reservoir.py
+++ 1_Gas_Reservoir.py
@@ -39,4 +39,15 @@ Q = st.number_input("Enter rate (in km3/d): ")
 st.button("Calculate IPR for test data")
 
 st.markdown("ahora hay que ver como incorporar muchos tests... se podria poner un boton add test data y que te de las ventanas para cargar datos... algo asi...y como hacer para darle play al codigo que hace la IPR y vincular todo con lo anterior")
-st.snow()
+
+with st.form("my_form"):
+   st.write("Inside the form")
+   slider_val = st.slider("Form slider")
+   checkbox_val = st.checkbox("Form checkbox")
+
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
