b53c1e87d1da77480e2f68e926004b2d4f09de3f
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-27 16:16:30 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -2,5 +2,9 @@ import streamlit as st
 
 st.title("IPR Calculation")
 
+intro=('''This streamlit app contains a pyhton script that will allow you to perform an IPR Calculation for both an oil
+and gas reservoir. It collects user input test data to perform a curve fitting using scipy.optimize.curve_fit, 
+fitting the Inflow Performance Relationship curve to the data.''')
+
 st.page_link("1_Gas_Reservoir.py", label="Gas Reservoir")
 st.page_link("2_Oil_Reservoir,py", label="Oil Reservoir")
