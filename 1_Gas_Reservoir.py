afeb2ccd23feac7f62ee71bafeda0a540dd56134
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-27 14:48:56 +0100

    Update 1_Gas_Reservoir.py

--- 1_Gas_Reservoir.py
+++ 1_Gas_Reservoir.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+from scipy.optimize import minimize
 from tabulate import tabulate
 
 st.title("Inflow Performance Curve (IPR) Calculation")
@@ -28,15 +28,14 @@ st.markdown(forcheimer)
 st.divider()
 
 # Definition of the quadratic curve equation (Inflow Performance Relationship - IPR)
-def curve_IPR(Q, a, b):
-    return np.sqrt(-a * Q ** 2 - b * Q + Pws**2)
+def curve_IPR(Q, params):
+    a, b, Pws = params
+    return np.sqrt(-a * Q ** 2 - b * Q + Pws ** 2)
 
-Pws = st.number_input("Reservoir Pressure (bar)")
 # Function to collect input data
 def collect_data():
     st.header("Input Test Data")
     rows = st.number_input("Number of Test Data", min_value=1, value=1)
-
 
     # Create an empty list to store input values
     data = []
@@ -48,76 +47,87 @@ def collect_data():
         Pwf = st.number_input("Flowing Bottomhole Pressure (bar)", key=f"Pwf_{i}")
         Q = st.number_input("Rate (km3/d)", key=f"Q_{i}")
 
-        data.append((date, comment, Pwf, Q))
+        data.append([date, comment, Pwf, Q])
 
-    return Pws, data
+    return data
 
 def main():
-
-
     # Load test data
-    Pws, data = collect_data()
+    data = collect_data()
 
     if data:
-        if st.button('Run IPR Curve Fitting'):
-            # Convert rate from km3/d to Sm3/day
-            for i in range(len(data)):
-                data[i] = (data[i][0], data[i][1], data[i][2],  data[i][3] * 1e3)
+        # Extract Pws
+        Pws = data[0][2]
 
-            # For Pws
-            data2 = [(Pws,0)]
-            
-            # Convert data into arrays
-            Q_data = np.array([d[3] for d in data]+[d[1] for d in data2])
-            P_data = np.array([d[2] for d in data]+[d[0] for d in data2])
+        # Define error function to minimize
+        def error_function(params):
+            a, b, Pws = params
+            errors = np.log(np.abs(Pws ** 2 - a * Q_data ** 2 - b * Q_data)) - np.log(np.abs(Pwf_data ** 2))
+            squared_errors = np.sum(errors ** 2)
+            scaled_squared_errors = squared_errors * 1000
+            return scaled_squared_errors
 
-            # Perform curve fitting
-            initial_guess = [3.75e-9, 4.17e-4]  # Initial guess for the parameters a, b, and c
-            bounds = ([0, 0], [np.inf, np.inf])  # Bounds for the parameters
-            params, _ = curve_fit(curve_IPR, Q_data, P_data, p0=initial_guess, bounds=bounds)
-            a_fit, b_fit = params
-            
-            st.header("Fitted Parameters:")
-            col1, col2 = st.columns(2)
-            col1.metric(label=f":red[a (bar2/(Sm3/day)2)]", value=f"{a_fit:.2e}")
-            col1.metric(label=f":green[b (bar2/Sm3/day)]", value=f"{a_fit:.2e}")
-            col2.metric(label=f":black[Reservoir Pressure (bar)]", value=f"{Pws:.2f}")
+        # Extract Pwf and Q data
+        Pwf_data = np.array([d[2] for d in data])
+        Q_data = np.array([d[3] for d in data])
 
+        # Perform optimization
+        initial_guess = [3.75e-9, 4.17e-4, Pws]
+        bounds = [(0, np.inf), (0, np.inf), (Pws - 1e-9, Pws + 1e-9)]
+        result = minimize(error_function, initial_guess, bounds=bounds)
 
-            # AOF Calculation
-            # Bhaskara’s formula to find positive root
-            discriminant = b_fit ** 2 + 4 * a_fit * Pws ** 2
-            if discriminant >= 0:
-                AOF = (-b_fit + np.sqrt(discriminant)) / (2 * a_fit)
-                col2.metric(label=f":blue[AOF (km3/d)]",value=f"{AOF/1000:.2f}")
-            else:
-                st.write("No real roots exist.")
-            
-            # Range of points for extrapolation of the curve
-            Q_range = np.linspace(0, AOF, 500)
-            Pwf_fit = curve_IPR(Q_range, a_fit, b_fit)
+        # Extract optimized parameters
+        a_fit, b_fit, Pws_fit = result.x
 
-            # Plot
-            st.subheader("IPR Curve")
-            fig, ax = plt.subplots()
-            ax.scatter(Q_data/1000, P_data, color='red', label='Test Data')
-            ax.plot(Q_range/1000, Pwf_fit, color='blue', label='Fitted Curve')
-            ax.set_xlabel('Rate (km$^3$/ d)')
-            ax.set_ylabel('Pressure (bar)')
-            ax.set_title('Pressure vs Rate')
-            ax.legend()
-            ax.grid(True)
+        st.header("Fitted Parameters:")
+        col1, col2 = st.columns(2)
+        col1.metric(label=f":red[a (bar2/(Sm3/day)2)]", value=f"{a_fit:.2e}")
+        col1.metric(label=f":green[b (bar2/Sm3/day)]", value=f"{b_fit:.2e}")
+        col2.metric(label=f":black[Reservoir Pressure (bar)]", value=f"{Pws_fit:.2f}")
 
-            # Set the limits of the axes to ensure the plot starts at (0, 0)
-            ax.set_xlim(left=0, auto=True)
-            ax.set_ylim(bottom=0, auto=True)
+        # AOF Calculation
+        discriminant = b_fit ** 2 + 4 * a_fit * Pws_fit ** 2
+        if discriminant >= 0:
+            AOF = (-b_fit + np.sqrt(discriminant)) / (2 * a_fit)
+            col2.metric(label=f":blue[AOF (km3/d)]", value=f"{AOF / 1000:.2f}")
+        else:
+            st.write("No real roots exist.")
 
-            st.pyplot(fig)
+        # Range of points for extrapolation of the curve
+        Q_range = np.linspace(0, AOF, 500)
+        Pwf_fit = curve_IPR(Q_range, [a_fit, b_fit, Pws_fit])
 
-    
+        # Plot
+        st.subheader("IPR Curve")
+        fig, ax = plt.subplots()
+        ax.scatter(Q_data, Pwf_data, color='red', label='Test Data')
+        ax.plot(Q_range, Pwf_fit, color='blue', label='Fitted Curve')
+        ax.set_xlabel('Rate (km$^3$/ d)')
+        ax.set_ylabel('Pressure (bar)')
+        ax.set_title('Pressure vs Rate')
+        ax.legend()
+        ax.grid(True)
+
+        # Set the limits of the axes to ensure the plot starts at (0, 0)
+        ax.set_xlim(left=0, auto=True)
+        ax.set_ylim(bottom=0, auto=True)
+
+        st.pyplot(fig)
 
     else:
         st.write("No data provided.")
 
 if __name__ == "__main__":
     main()
+error=(''' **Error function to minimize by solver during curve fitting: $ ∑ [ln(Pws^2 - a \cdot Q^2 - b \cdot Q  ) -  ln( Pwf^2)]^2\cdot 1000 $**
+
+*NOTE: By taking the logarithm of the difference between the model prediction and the test data, we can effectively 
+handle a wide range of values and mitigate the influence of outliers. Squaring the differences ensures that all values are positive,
+facilitating optimization algorithms to converge efficiently. Additionally, multiplying the squared differences by a large number amplifies 
+their impact during optimization, improving the precision of the solver and enabling finer adjustments to the model parameters.* ''')
+
+st.divider()
+st.markdown(error)
+st.divider()
+st.sidebar()
+
