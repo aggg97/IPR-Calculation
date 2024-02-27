244efcf43d587372e89c7d8e27f40bbcb9cf5667
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-27 16:28:13 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -6,5 +6,5 @@ intro=('''This streamlit app contains a pyhton script that will allow you to per
 and gas reservoir. It collects user input test data to perform a curve fitting using scipy.optimize.curve_fit, 
 fitting the Inflow Performance Relationship curve to the data.''')
 
-st.page_link("1_Gas_Reservoir.py", label="Gas Reservoir")
-st.page_link("2_Oil_Reservoir,py", label="Oil Reservoir")
+st.page_link("pages/1_Gas_Reservoir.py", label="Gas Reservoir")
+st.page_link("pages/2_Oil_Reservoir,py", label="Oil Reservoir")
