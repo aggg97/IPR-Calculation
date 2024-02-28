241c073a8c8b8509409d15204be7e2f0ad26b7d8
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-28 09:38:18 +0100

    Update 1_Gas_Reservoir.py

--- pages/1_Gas_Reservoir.py
+++ pages/1_Gas_Reservoir.py
@@ -121,11 +121,11 @@ def main():
 if __name__ == "__main__":
     main()
  
-st.markdown=(''' **Error function to minimize by solver during curve fitting: ''')
+st.markdown(''' **Error function to minimize by solver during curve fitting: ''')
 
 st.latex(r''' ∑ [ln(Pws^2 - a \cdot Q^2 - b \cdot Q  ) -  ln( Pwf^2)]^2\cdot 1000 ''')
 
-st.markdown=('''*NOTE: By taking the logarithm of the difference between the model prediction and the test data, we can effectively 
+st.markdown('''*NOTE: By taking the logarithm of the difference between the model prediction and the test data, we can effectively 
 handle a wide range of values and mitigate the influence of outliers. Squaring the differences ensures that all values are positive,
 facilitating optimization algorithms to converge efficiently. Additionally, multiplying the squared differences by a large number amplifies 
 their impact during optimization, improving the precision of the solver and enabling finer adjustments to the model parameters.* ''')
