bf6111916fa31a00140db52b7fd189e79e8b9b39
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-28 09:11:50 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -10,7 +10,9 @@ st.markdown(intro)
 col1, col2 = st.columns(2)
 
 with col1:
-    st.header('<a href="pages/1_Gas_Reservoir.py" style="color:red">Gas Reservoir</a>', unsafe_allow_html=True)
+    st.subheader("Gas Reservoir")
+    st.markdown("[Link to Gas Reservoir](pages/1_Gas_Reservoir.py)")
 
 with col2:
-    st.header('<a href="pages/2_Oil_Reservoir.py" style="color:green">Oil Reservoir</a>', unsafe_allow_html=True)
+    st.subheader("Oil Reservoir")
+    st.markdown("[Link to Oil Reservoir](pages/2_Oil_Reservoir.py)")
