fda288ccacd17ac541c20c2b7d1612ca2fa5e0b3
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-28 09:09:09 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -2,16 +2,17 @@ import streamlit as st
 
 st.title("IPR Calculation")
 
-intro=('''This streamlit app contains a pyhton script that will allow you to perform an IPR Calculation for both an oil
+intro = '''This Streamlit app contains a Python script that will allow you to perform an IPR Calculation for both an oil
 and gas reservoir. It collects user input test data to perform a curve fitting using scipy.optimize.curve_fit, 
-fitting the Inflow Performance Relationship curve to the data.''')
-
+fitting the Inflow Performance Relationship curve to the data.'''
 st.markdown(intro)
 
-col1, col2= st.columns(2)
+col1, col2 = st.columns(2)
 
 with col1:
-  st.page_link("pages/1_Gas_Reservoir.py",color=red, label="Gas Reservoir")
+    st.markdown('<style> .red-text { color: red; } </style>', unsafe_allow_html=True)
+    st.page_link("pages/1_Gas_Reservoir.py", label="Gas Reservoir", class_name="red-text")
 
 with col2:
-  st.page_link("pages/2_Oil_Reservoir.py",color=green,  label="Oil Reservoir")
+    st.markdown('<style> .green-text { color: green; } </style>', unsafe_allow_html=True)
+    st.page_link("pages/2_Oil_Reservoir.py", label="Oil Reservoir", class_name="green-text")
