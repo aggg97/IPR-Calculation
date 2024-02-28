d790902ca9cf3629c86264482f2678d512a2b3ca
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-28 16:16:29 +0100

    Update 2_Oil_Reservoir.py

--- pages/2_Oil_Reservoir.py
+++ pages/2_Oil_Reservoir.py
@@ -96,20 +96,23 @@ def main():
         
         st.pyplot(fig)
 
+        st.divider()
+        st.write("Error function to minimize by solver during curve fitting:")
+        
+        st.latex(r''' ∑ [ln(AOF*[1-0.2\frac{Pwf}{Pws}-0.8(\frac{Pwf}{Pws})^{2}]) -  ln( Q)]^2\cdot 1000 ''')
+        
+        st.write('''*NOTE: By taking the logarithm of the difference between the model prediction and the test data, 
+        we can effectively handle a wide range of values and mitigate the influence of outliers. 
+        Squaring the differences ensures that all values are positive, facilitating optimization algorithms
+        to converge efficiently. Additionally, multiplying the squared differences by a large number amplifies their impact during optimization, 
+        improving the precision of the solver and enabling finer adjustments to the model parameters.*''')
+        
+        st.divider()
+
+
+
 
 if __name__ == "__main__":
     main()
     
-st.divider()
-st.write("Error function to minimize by solver during curve fitting:")
-
-st.latex(r''' ∑ [ln(AOF*[1-0.2\frac{Pwf}{Pws}-0.8(\frac{Pwf}{Pws})^{2}]) -  ln( Q)]^2\cdot 1000 ''')
-
-st.write('''*NOTE: By taking the logarithm of the difference between the model prediction and the test data, 
-we can effectively handle a wide range of values and mitigate the influence of outliers. 
-Squaring the differences ensures that all values are positive, facilitating optimization algorithms
-to converge efficiently. Additionally, multiplying the squared differences by a large number amplifies their impact during optimization, 
-improving the precision of the solver and enabling finer adjustments to the model parameters.*''')
-
-st.divider()
 
