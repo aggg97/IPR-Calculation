0c1dbe859ff61aa2d5e1445ccd60953c9ba14dba
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-28 09:19:44 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -5,21 +5,17 @@ st.title("IPR Calculation")
 intro = '''This Streamlit app contains a Python script that will allow you to perform an IPR Calculation for both an oil
 and gas reservoir. It collects user input test data to perform a curve fitting using scipy.optimize.curve_fit, 
 fitting the Inflow Performance Relationship curve to the data.'''
+
 st.markdown(intro)
-
-st.page_link("pages/1_Gas_Reservoir.py", label="Gas Reservoir", icon="1️⃣")
-st.page_link("pages/2_Oil_Reservoir.py", label="Oil Reservoir", icon="1️⃣")
-
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown('<h3 style="color:red;">Gas Reservoir</h3>', unsafe_allow_html=True)
-    st.markdown("[click here](pages/1_Gas_Reservoir.py)")
+    st.page_link("pages/1_Gas_Reservoir.py", label="Gas Reservoir", color=red)
 
 with col2:
-    st.markdown('<h3 style="color:green;">Oil Reservoir</h3>', unsafe_allow_html=True)
-    st.markdown("[clcik here](pages/2_Oil_Reservoir.py)")
+    st.page_link("pages/2_Oil_Reservoir.py", label="Oil Reservoir", color=green)
+   
     
 
 st.markdown("add photos with what you can do")
