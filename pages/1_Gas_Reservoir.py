3e2bf41e1271d022e620376d55b2d339798b94a8
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-28 10:02:28 +0100

    Update 1_Gas_Reservoir.py

--- pages/1_Gas_Reservoir.py
+++ pages/1_Gas_Reservoir.py
@@ -8,8 +8,6 @@ st.title("Gas Reservoir")
 
 st.header("Forcheimer Reservoir Model: ")
 
-st.write("The Forcheimer equation expresses the inflow performance in terms of turbulent (a coefficient) and non turbulent (b coefficient) pressure drop coefficients expressed as:")
-
 st.latex(r'''\Delta P^2 = Pws ^2- Pwf^2 = a \cdot Q^2 + b \cdot Q ''')
 
 st.latex(r'''a \cdot Q^2 + b \cdot Q  - (Pws ^2- Pwf^2 ) = 0''')
