afa23d6f462056cbf3f29842b431c7fd7d8eadd8
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-29 15:21:38 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -12,14 +12,17 @@ col1, col2,col3 = st.columns(3)
 
 with col1:
     st.markdown('<h3 style="color:red;">Gas Reservoir</h3>', unsafe_allow_html=True)
+    st.markdown("IPR Calculation using Forcheimer model")
     st.page_link("pages/1_Gas_Reservoir.py", label="click here")
 
 with col2:
     st.markdown('<h3 style="color:green;">Oil Reservoir</h3>', unsafe_allow_html=True)
+    st.markdown("IPR Calculation using Vogel quadratic equation")
     st.page_link("pages/2_Oil_Reservoir.py", label="click here")
 
 with col3:
     st.markdown('<h3 style="color:blue;">Multiwell IPR Calculation</h3>', unsafe_allow_html=True)
+    st.markdown("By uploading a .csv file with test data for different well, IPR calculation will be done with Forcheimer or Vogel taking into account reservoir fluid type")
     st.page_link("pages/3_Multiwell_IPR_Calculation.py", label="click here")
     
 
