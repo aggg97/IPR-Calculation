738afdc72094cd0bb8d80dc239117430644abd66
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-29 15:26:03 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -22,7 +22,7 @@ with col2:
 
 with col3:
     st.markdown('<h3 style="color:blue;">Multiwell IPR Calculation</h3>', unsafe_allow_html=True)
-    st.markdown("By uploading a .csv file with test data for different wells, IPR calculation will be done with either Forcheimer's or Vogel's IPR cuadratic equation")
+    st.markdown("By uploading a .csv file with test data from different wells, IPR calculation will be done with either Forcheimer's or Vogel's IPR cuadratic equation")
     st.page_link("pages/3_Multiwell_IPR_Calculation.py",label="click here")
 
 st.divider()
