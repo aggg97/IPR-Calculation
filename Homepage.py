a8eb0382de4d372aaf242067f5f18b512422363d
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2024-02-29 15:23:55 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -13,16 +13,16 @@ col1, col2,col3 = st.columns(3)
 with col1:
     st.markdown('<h3 style="color:red;">Gas Reservoir</h3>', unsafe_allow_html=True)
     st.markdown("IPR Calculation using Forcheimer model")
-    st.page_link("pages/1_Gas_Reservoir.py", label="click here")
+    st.page_link("pages/1_Gas_Reservoir.py", label="<b>click here</b>")
 
 with col2:
     st.markdown('<h3 style="color:green;">Oil Reservoir</h3>', unsafe_allow_html=True)
     st.markdown("IPR Calculation using Vogel quadratic equation")
-    st.page_link("pages/2_Oil_Reservoir.py", label="click here")
+    st.page_link("pages/2_Oil_Reservoir.py", label="<b>click here</b>")
 
 with col3:
     st.markdown('<h3 style="color:blue;">Multiwell IPR Calculation</h3>', unsafe_allow_html=True)
     st.markdown("By uploading a .csv file with test data for different wells, IPR calculation will be done with either Forcheimer's or Vogel's IPR cuadratic equation")
-    st.page_link("pages/3_Multiwell_IPR_Calculation.py", label="click here")
+    st.page_link("pages/3_Multiwell_IPR_Calculation.py", label="<b>click here</b>")
 
 st.divider()
